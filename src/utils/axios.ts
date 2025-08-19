dc11c260204a36074e8e85d91c5dd9b8ea977bc6
Alex Romeu <alexxromeu@gmail.com>
2025-08-19 14:19:02 -0500

    last

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.1.70:3000", 
+  baseURL: "https://chatapp-backend-zo7f.onrender.com", 
   withCredentials: true
 });
 
