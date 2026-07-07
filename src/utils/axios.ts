6064e965726db9f984551d89373d024b2291e978
Alexromeu <alexxromeu@gmail.com>
2026-07-07 14:07:34 -0500

    fix backend url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://chatapp-backend-zo7f.onrender.com",
+  baseURL: "https://btoewcqasarvrysdfbef.supabase.co",
   // baseURL: "http://192.168.1.70:3000",
   withCredentials: true
 });
