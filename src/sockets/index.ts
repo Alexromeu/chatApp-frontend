a5f8e24a0559b3acec87fabc61c45f9334eefe00
Alexromeu <alexxromeu@gmail.com>
2026-07-07 02:02:56 -0500

    add_new_backend

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from "socket.io-client";
-const URL = "https://chatapp-backend-zo7f.onrender.com";
+const URL = "https://btoewcqasarvrysdfbef.supabase.co";
 // const URL = "http://192.168.1.70:3000";
 
 export const socket: Socket = io(URL, {
